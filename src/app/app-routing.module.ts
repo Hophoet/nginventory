99e2d7d8ab482dca0e9ea7027f476d33025937aa
hophoet <hophoet@gmail.com>
2021-03-18 18:37:45 +0000

    (route) - set the app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent }  from './auth/auth.component';
+import { CoreComponent} from './core/core.component'
+import { UpdateComponent} from './update/update.component'
+import { InsertComponent} from './insert/insert.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AuthComponent},
+  {path: 'home', component: CoreComponent},
+  {path:'update/:type/:id', component:UpdateComponent },
+  {path:'insert/:type', component:InsertComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
